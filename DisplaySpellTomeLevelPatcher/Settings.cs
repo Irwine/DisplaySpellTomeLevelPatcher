05c193ef74df26de98fa27298570a25a7e634e28
Zehabys <143323059+Zehabys@users.noreply.github.com>
2023-08-27 14:49:26 +0200

    Update Settings.cs FR

--- DisplaySpellTomeLevelPatcher/Settings.cs
+++ DisplaySpellTomeLevelPatcher/Settings.cs
@@ -1,15 +1,36 @@
 ﻿using Mutagen.Bethesda.Synthesis.Settings;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DisplaySpellTomeLevelPatcher
 {
     public class Settings
     {
-        [SynthesisTooltip(@"Choisissez votre propre format ici ! Les variables disponibles sont : <level> (ex. Adepte), <spell> (ex. Clairvoyance), <plugin> (ex. Skyrim), <school> (ex. Altération). Le format par défaut est : Livre de sort (<level>) - <spell>")]
+        [SynthesisTooltip(@"Choisissez votre propre format ici ! Les variables disponibles sont : <level> (ex. Adepte), <spell> (ex. Clairvoyance), <plugin> (ex. Skyrim), <mod> (nom du mod au lieu du nom du plugin, ex. Forgotten Magic Redone), <school> (ex. Alteration). Le format par défaut est : Livre de sort (<level>) - <spell>")]
         public string Format { get; set; } = "Livre de sort (<level>) - <spell>";
+
+        [SynthesisTooltip(@"Spécifiez votre propre format pour les noms de mod (<mod>) ici ! Lorsqu'un nom de plugin n'est pas trouvé ici, le patcher essaiera de convertir automatiquement le nom du plugin en nom de mod - les résultats peuvent varier.")]
+        public Dictionary<string, string> PluginModNamePairs { get; set; } = new()
+        {
+            { "Skyrim.esm", "Skyrim" },
+            { "Dawnguard.esm", "Dawnguard" },
+            { "Dragonborn.esm", "Dragonborn" },
+            { "HearthFires.esm", "HearthFires" },
+            { "Apocalypse - Magic of Skyrim.esp", "Apocalypse" },
+            { "Arcanum.esp", "Arcanum" },
+            { "Triumvirate - Mage Archetypes.esp", "Triumvirate" },
+            { "ForgottenMagic_Redone.esp", "Forgotten Magic Redone" },
+            { "Phenderix Magic Evolved.esp", "Phenderix Magic Evolved" },
+            { "ShadowSpellPackage.esp", "Shadow Spell Package" },
+            { "PathOfTheAntiMage.esp", "Path of the Anti-Mage" }
+        };
+
+        [SynthesisTooltip(@"Ce sont les noms de niveau qui seront utilisés pour <level>. Vous pouvez éventuellement les raccourcir ou les remplacer par un autre nom ici. Par défault : Novice, Apprenti, Adepte, Expert, Maître")]
+        public List<string> LevelNames { get; set; } = new() {
+            "Novice",
+            "Apprenti",
+            "Adepte",
+            "Expert",
+            "Maître"
+        };
     }
 }
